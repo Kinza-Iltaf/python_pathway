f68489c66a6d02ab98124ca56e6e9a844d78d3bd
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-08-29 23:44:27 +0500

    Addition Of Day_04 Notes And Codes

---
+++ Day_04/Day_04_PracticeCode.py
@@ -0,0 +1,26 @@
+# Task : 01
+# Create a dictionary that store the student related data and also print the dictionary type too.
+
+student =  {
+   " name" :"john",
+    "class" : 5,
+    "grade" :"A",
+}
+print(student)
+print(type(student))
+
+
+# Task : 02
+# create a dictionary type variable and access its first two elements.
+
+profile = {
+    "Name" : "Sara",
+    "Total_Posts" : 107,
+    "Followers"    : 358,
+    "Following"   : 876,
+    "is_active"   : "True",
+}
+
+print("Here is the First Two elements of Profile(dic):")
+print(profile["Name"])
+print(profile["Total_Posts"])
