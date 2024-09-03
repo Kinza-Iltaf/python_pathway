b76dfa985f1cb54c97903494ee6f15ec21888f57
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-09-03 17:59:30 +0500

    Loops notes and practice questions

---
+++ Day_05_Loops/Practice Question.py
@@ -0,0 +1,37 @@
+# Task : 01
+# print your name 10 times on screen:
+i = 1
+while i<10:
+        print("Lorenzo")
+        i +=1
+# Task : 02
+# print no from 1 to 50
+
+count = 0
+while count <= 50:
+    print(count)
+    count+=1
+
+# Task : 03
+# print no from 1 to 50 in reverse order:
+
+count = 50
+while count >=0:
+        print(count)
+        count-=1
+
+# Task : 04
+# print the table of 3: 3 * 1 = 3.... or you can take a number from user too
+tab_num = int ( input ( " Enter the number of which you want to print the table:"))
+
+count = 1
+while count <= 10 :
+       print(tab_num, "*", count , "=", tab_num * count)
+       count+=1
+
+# Task : 05
+
+       
+
+
+
