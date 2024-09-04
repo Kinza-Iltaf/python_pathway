4732b0751b3850fc6f2362da5a8b6e6a1d5969fd
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-09-04 23:56:12 +0500

    addition of new problems

--- Day_05_Loops/Practice Question.py
+++ Day_05_Loops/Practice Question.py
@@ -169,3 +169,12 @@ for val in range(0 , num+1):
 
 print("sum of natural number till ",num, "is:", sum)
 
+
+# Task : 13
+# Take a number from user as input and find its factorial
+num = int(input("Enter the last number:"))
+fac = 1   
+for val in range(1 , num+1):
+    fac *= val  
+
+print("factorial of  number ",num, "is:", fac)
--- Day_05_Loops/test.py
+++ Day_05_Loops/test.py
@@ -1,6 +1,7 @@
+# Take a number from user as input and find its factorial
 num = int(input("Enter the last number:"))
-sum = 0   
-for val in range(0 , num+1):
-    sum += val  
+fac = 1   
+for val in range(1 , num+1):
+    fac *= val  
 
-print("sum of natural number till ",num, "is:", sum)
+print("factorial of  number ",num, "is:", fac)
