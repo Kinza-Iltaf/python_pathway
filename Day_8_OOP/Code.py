1bbe5a32d77e7d6a62dde9bc06b0b52342bb3086
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-09-10 22:38:32 +0500

    New practice questions

---
+++ Day_8_OOP/Code.py
@@ -0,0 +1,12 @@
+# code
+
+# Problem:
+# Create a class called Dog that has two attributes: age and breed. Then, create an instance of the class and access these attributes using the instance.
+class Dog:
+    name = "Blesky"
+    age = 12
+d1 = Dog
+print("Dog Name:",d1.name)  
+print("Dog Age:",d1.age) 
+
+
