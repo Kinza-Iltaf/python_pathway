aaed34e5c7799802cfcd06ffb901944a2d3beede
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-09-11 23:55:15 +0500

    updation

--- Day_8_OOP/Code.py
+++ Day_8_OOP/Code.py
@@ -9,7 +9,25 @@ class Dog:
         print("Here is the constructor:")
 
 
-Dog1 = Dog()
+Dog1 = Dog() #here we don't need to class the class because of constructor.
+
+
+# problem
+# Create a class called Car that has two attributes: brand and year. Use a constructor to initialize these attributes when an object is created. Then, create an instance of the class and print out the car's brand and year.
+
+class Car:
+   
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+
+car1= Car("mecedes",2010)  
+
+print(car1.brand)
+print(car1.year)    
+
+        
+
 
 
 
--- Day_8_OOP/test.py
+++ Day_8_OOP/test.py
@@ -1,1 +1,12 @@
 # test file
+
+class Car:
+   
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+
+car1= Car("mecedes",2010)  
+
+print(car1.brand)
+print(car1.year)
