acfa671cd579f5b492f662230ec21e83245d0c42
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-09-12 23:57:57 +0500

    updation

--- Day_8_OOP/Code.py
+++ Day_8_OOP/Code.py
@@ -24,7 +24,29 @@ class Car:
 car1= Car("mecedes",2010)  
 
 print(car1.brand)
-print(car1.year)    
+print(car1.year)   
+
+# Problem:
+# Create a class called Book that has three attributes: title, author, and pages. Use a constructor to initialize these attributes when an object is created. Add a method called is_long() that checks if the book has more than 100 pages. If it does, return True, otherwise return False.
+
+class Book:
+    def __init__(self, title, author,pages) :
+
+       self.title = title
+       self.author = author
+       self.pages = pages
+    def is_long(self):
+        if self.pages > 100:
+            return True
+        else:
+            return False
+        
+book = Book("Halim", "nimra ahmad", 1203)
+print(book.title)
+print(book.author)
+print(book.pages)
+value = book.is_long()
+print("checking for number of pages",value )
 
         
 
--- Day_8_OOP/test.py
+++ Day_8_OOP/test.py
@@ -1,12 +1,25 @@
 # test file
 
-class Car:
-   
-    def __init__(self, brand, year):
-        self.brand = brand
-        self.year = year
+# Problem:
+# Create a class called Book that has three attributes: title, author, and pages. Use a constructor to initialize these attributes when an object is created. Add a method called is_long() that checks if the book has more than 100 pages. If it does, return True, otherwise return False.
 
-car1= Car("mecedes",2010)  
+class Book:
+    def __init__(self, title, author,pages) :
 
-print(car1.brand)
-print(car1.year)
+       self.title = title
+       self.author = author
+       self.pages = pages
+    def is_long(self):
+        if self.pages > 100:
+            return True
+        else:
+            return False
+        
+book = Book("Halim", "nimra ahmad", 1203)
+print(book.title)
+print(book.author)
+print(book.pages)
+value = book.is_long()
+print("checking for number of pages",value )
+        
+
