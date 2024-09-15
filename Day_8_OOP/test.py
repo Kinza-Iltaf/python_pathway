b519f80d29bbb44c8ccf51c7d00e201e6d2ad45c
Kinza-Iltaf <kanzailtaf@gmail.com>
2024-09-15 23:58:49 +0500

    addition of new practice problems

--- Day_8_OOP/test.py
+++ Day_8_OOP/test.py
@@ -1,21 +1,50 @@
-class Car:
-    @staticmethod
-    def start():
-        print("Car started...")
+# Problem: Employee and Manager
+# Create a class Employee that has the following:
+# Attributes: name, salary
+# Methods:
+# get_details() - prints the employee's details
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def print_details(self, name,salary):
+        print("Employee name:",self.name) 
+        print("Employee salary:",self.salary)   
+class Manager(Employee):
+        def __init__(self, name, salary , department):
+             super().__init__(name, salary)
+             self.dep = department
 
-    @staticmethod
-    def stop():
-        print("Car stopped...")
+        def get_details(self):  
+               print("Manager name:",self.name) 
+               print("Manager salary:",self.salary)  
+               print("Manager Department:",self.dep) 
 
-class Toyota(Car):
-    def __init__(self,color):
-        self.color = color
-        print("car color is:",self.color)
+manager = Manager("karan", 1248,"HR")  
+manager.get_details()  
 
+        
+        
 
-car = Toyota("Blue")
-car.start()
-car.stop()
+       
+
+# Create a class Manager that inherits from Employee and adds the following:
+
+# Attributes: department
+# Methods:
+# Override get_details() to include the department in the output.
+# Objective:
+# Implement both classes.
+# Create objects of both classes and call their get_details() methods.
+# Example:
+# python
+# Copy code
+# Output should include:
+# Employee Name: John
+# Salary: 50000
+# Manager Name: Sarah
+# Salary: 90000
+# Department: HR
 
 
 
